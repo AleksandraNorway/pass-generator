e0176f6a437a079a95b48b3c76a735dd0adb05f7
Aleksandra Ilic <95101761+AleksandraNorway@users.noreply.github.com>
2024-01-29 20:48:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
-let homeScore = document.getElementById("home-score")
-let guestScore = document.getElementById("guest-score")
-let score1 = 0;
-let score2 = 0;
-
-
-function addScoreHome(points) {
-   score1 += points;
-   homeScore.innerText = score1;
-   updateScoreStyles()
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let randNumberOne = document.getElementById("rand-number1")
+let randNumberTwo = document.getElementById("rand-number2")
+let generatePass = document.querySelector("button")
+let inputNumber = document.getElementById("input-number")
+ let alertTextError = document.getElementById("alert-text-error")
+ let alertText = document.getElementById("alert-text")
+  
+generatePass.addEventListener('click', function(){
+    randNumberOne.textContent = ""
+    randNumberTwo.textContent = ""
+    if(inputNumber.value < 6) {
+       alertTextError.innerHTML = "Amount to small, try higher number...!"
+    } else {
+          for (let i=0; i < inputNumber.value; i++) {
+         let newPassOne = Math.floor(Math.random() * characters.length)
+         let newPassTwo = Math.floor(Math.random() * characters.length)
+         randNumberOne.textContent += characters[newPassOne]
+         randNumberTwo.textContent += characters[newPassTwo]
+         alertText.innerHTML = "Click on password to copy"
+         alertTextError.innerHTML = ""
+    
+   }  
    
-}
-function addScoreGuest(points) {
-   score2 += points;
-   guestScore.innerText = score2;
-   updateScoreStyles()
+    }
+   
+  
+})
+
+function copyToClipboard(element) {
+    if (element.textContent) {
+       navigator.clipboard.writeText(element.textContent)
+            .then(() => alertText.innerHTML = "<span>Password copied to clipboard!</span>")
+            .catch(err => {
+                console.error("Error copying text: ", err);
+                alertTextError.innerHTML = "Failed to copy password.";
+            });
+    }
 }
 
-function updateScoreStyles() {
-if (score1 > score2) {
-    homeScore.classList.add('highliter')
-    guestScore.classList.remove('highliter')
-} else if (score1 < score2)  {
-     guestScore.classList.add('highliter')
-     homeScore.classList.remove('highliter')
-} else {
-    // Set the border color for the else condition
-     guestScore.classList.remove('highliter')
-     homeScore.classList.remove('highliter') // Replace 'anotherColor' with the color of your choice
-}
-}
-// Initialize styles on page load
-updateScoreStyles();
- 
- function newGame() {
-     score1 = 0;
-     score2 = 0;
-     homeScore.innerText = "0";
-     guestScore.innerText = "0";
-     
- }
+randNumberOne.addEventListener('click', function() {
+    copyToClipboard(randNumberOne);
+});
+
+randNumberTwo.addEventListener('click', function() {
+    copyToClipboard(randNumberTwo);
+});
+
+
+
